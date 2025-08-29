fad58e2e4cabfc4cc2f7465ec57151cb90f56024
susushi555 <143075979+susushi555@users.noreply.github.com>
2025-08-29 15:36:30 +0900

    修正
    修正

--- map.rb
+++ map.rb
@@ -1,43 +1,29 @@
-class Map
-    attr_accessor :map_date
-    def initialize(filename,mapimage,target=Window)
-        @mapimage, @target = mapimage, target
-        @mapdata = []
-        File.open(filename, "rt") do |fh|
-            lines = fh.readlines
-            lines.each do |line|
-                ary = []
-                line.chomp.each_char do |o|
-                    if o != "x"
-                        ary << o.to_i
-                    else
-                        ary << nil
-                    end
-                end
-                @mapdata << ary
-            end
-        end
-    end
-    def [](x, y)
-        @mapdata[y % @mapdata.size][x % @mapdata[0].size]
-    end
+ class Map
+-    attr_accessor :map_date
++    # Expose the parsed map data for read-only access
++    attr_reader :mapdata
+     def initialize(filename,mapimage,target=Window)
+         @mapimage, @target = mapimage, target
+         @mapdata = []
+         File.open(filename, "rt") do |fh|
+             lines = fh.readlines
+             lines.each do |line|
+                 ary = []
+                 line.chomp.each_char do |o|
+                     if o != "x"
+                         ary << o.to_i
+                     else
+                         ary << nil
+                     end
+                 end
+                 @mapdata << ary
+             end
+         end
+     end
+     def [](x, y)
+         @mapdata[y % @mapdata.size][x % @mapdata[0].size]
+     end
 
-    def []=(x, y, v)
-        @mapdata[y % @mapdata.size][x % @mapdata[0].size] = v
-    end
-
-    def size_x
-        @mapdata[0].size
-    end
-
-    def size_y
-        @mapdata.size
-    end
-
-    def draw(x, y)
-        image = @mapimage[1]
-        @target.draw_tile(0, 0, @mapdata, @mapimage, x, y,
-                          (@target.width + image.width - 1) / image.width,
-                          (@target.height + image.height - 1) / image.height)
-    end
-end
+     def []=(x, y, v)
+         @mapdata[y % @mapdata.size][x % @mapdata[0].size] = v
+     end
